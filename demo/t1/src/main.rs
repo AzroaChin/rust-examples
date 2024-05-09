74cd51028c0e58904d302b1c1e88dedb7fd587d0
Liquidwe <lichengbing9027@gamil.com>
2024-05-09 22:49:05 +0800

    rust struct

--- demo/t1/src/main.rs
+++ demo/t1/src/main.rs
@@ -1,18 +1,45 @@
+use std::mem::size_of;
+
 #[derive(Debug)]
 struct Rectangular {
     width: u32,
     length: u32
 }
 
+impl Rectangular {
+    fn count(&self) -> u32 {
+        self.width * self.length
+    }
+
+    fn can_hold(&self, other: &Rectangular) -> bool {
+        self.width > other.width && self.length > other.length
+    }
+
+    fn square(size: u32) -> Rectangular {
+        Rectangular{
+            width: size,
+            length: size
+        }
+    }
+}
+
 fn main() {
-    let rect = Rectangular {
+    let rect1 = Rectangular {
         width: 30,
         length: 40
     };
-    let r = count(&rect);
-    println!("{:#?} -> {}", rect, r)
-}
+    let rect2 = Rectangular {
+        width: 10,
+        length: 50
+    };
+    let rect3 = Rectangular {
+        width: 20,
+        length: 20
+    };
+    println!("{}", rect1.can_hold(&rect2));
+    println!("{}", rect1.can_hold(&rect3));
 
-fn count(rect: &Rectangular) -> u32 {
-    rect.width * rect.length
+    // 关联函数: impl块里不把 self 作为第一个参数的函数叫关联函数（不是方法）
+    let s = Rectangular::square(20);
+    println!("{:#?}", s)
 }
