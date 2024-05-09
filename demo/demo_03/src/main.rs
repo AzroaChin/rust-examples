a32670fc6df453407a805bb0b602a85467753892
AzroaChin <961759593@qq.com>
2024-05-10 00:18:57 +0800

    rust struct

--- demo/demo_03/src/main.rs
+++ demo/demo_03/src/main.rs
@@ -24,7 +24,7 @@ fn value_in_cents(coin: Coin) -> u8 {
 }
 
 fn main() {
-    // 从enum变体中提取值
+    // 从 enum 变体中提取值
     let c = Coin::Quarter(UsState::NewYork);
     println!("{}", value_in_cents(c))
 }
